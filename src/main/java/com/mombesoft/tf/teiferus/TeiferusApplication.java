c16f8ec626b72c57e3eacce4ebfbb10b4d147bb0
Mamadou Yaya DIALLO <myaya.diallo@wuriguinee.com>
2020-10-18 22:38:06 +0000

    Maven check

---
+++ src/main/java/com/mombesoft/tf/teiferus/TeiferusApplication.java
@@ -0,0 +1,13 @@
+package com.mombesoft.tf.teiferus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeiferusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeiferusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mombesoft/tf/teiferus/TeiferusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mombesoft.tf.teiferus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeiferusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
